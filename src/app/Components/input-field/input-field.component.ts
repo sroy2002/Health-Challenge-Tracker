f21ef7cf5338f0a9323a627bd6e380afb31174e3
Suranjana Roy <roysupra75@gmail.com>
2025-01-30 21:01:26 +0530

    user input part is added

---
+++ src/app/Components/input-field/input-field.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-input-field',
+  templateUrl: './input-field.component.html',
+  styleUrl: './input-field.component.css',
+  // imports: [FormsModule, MatFormFieldModule, MatInputModule],
+})
+export class InputFieldComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './Components/navbar/navbar.component';
+import { InputFieldComponent } from './Components/input-field/input-field.component';
 @Component({
   selector: 'app-root',
   // imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports:[NavbarComponent]
+  imports:[NavbarComponent,InputFieldComponent],
+  standalone:true
 })
 export class AppComponent {
   title = 'healthTracker';
