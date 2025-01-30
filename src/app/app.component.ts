00bf83835fbc0e6cd6072ea2808a6c39b29f30a9
Suranjana Roy <roysupra75@gmail.com>
2025-01-30 19:55:09 +0530

    starting the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NavbarComponent } from './Components/navbar/navbar.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  // imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports:[NavbarComponent]
 })
 export class AppComponent {
   title = 'healthTracker';
